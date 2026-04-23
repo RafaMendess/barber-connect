77b196dbbcef43cd82d40b2b31284d8fe918e520
RafaMendess <rafasilvam220@gmail.com>
2026-04-22 23:07:06 -0300

    Initial commit: Barber Connect API structure

---
+++ src/test/java/com/projeto/barberconnect/BarberconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.barberconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
