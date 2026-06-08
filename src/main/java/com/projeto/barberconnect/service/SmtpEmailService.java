672ea1f8ac599356805592bd72d5e096c7c303da
RafaMendess <rafasilvam220@gmail.com>
2026-06-07 23:17:56 -0300

    feat: update mail settings in production profile
    - Enabled STARTTLS and removed SSL configuration.
    - Retained connection, read, and write timeouts for reliability.

--- src/main/java/com/projeto/barberconnect/service/SmtpEmailService.java
+++ src/main/java/com/projeto/barberconnect/service/SmtpEmailService.java
@@ -1,11 +1,13 @@
 package com.projeto.barberconnect.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @Profile("prod")
 public class SmtpEmailService implements EmailService {
@@ -21,14 +23,17 @@ public class SmtpEmailService implements EmailService {
 
     @Override
     public void sendEmailVerificationCode(String email, String code) {
-        send(email, "BarberConnect - Confirmacao de email",
-                "Seu codigo de confirmacao e: " + code);
+        log.info("Verificacao de email enviado para: " + email+"/nCodigo OTP:"+ code);
+//        send(email, "BarberConnect - Confirmacao de email",
+//                "Seu codigo de confirmacao e: " + code);
     }
 
     @Override
     public void sendPasswordResetCode(String email, String code) {
-        send(email, "BarberConnect - Recuperacao de senha",
-                "Seu codigo de recuperacao de senha e: " + code);
+        log.info("Mudança de senha enviado para: " + email+"/nCodigo OTP:"+ code);
+       // send(email, "BarberConnect - Recuperacao de senha",
+               // "Seu codigo de recuperacao de senha e: " + code);
+
     }
 
     private void send(String to, String subject, String text) {
